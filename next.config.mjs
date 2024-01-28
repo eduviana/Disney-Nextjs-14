d6e49f4180422e5dacee7a66e576f0db8ec16209
eduviana <eduardoviana83@gmail.com>
2024-01-28 16:32:34 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "image.tmdb.org",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "links.papareact.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: [
+//       "image.tmdb.org",
+//       "links.papareact.com",
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "image.tmdb.org",
+      "links.papareact.com",
+      // Agrega otros dominios según sea necesario
+    ],
+    loader: "default",
+  },
+};
 
 export default nextConfig;
